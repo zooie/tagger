f00abad4061fe21762f2f6311ffe3199db411692
Vik Singh <vik_k_singh@yahoo.com>
2009-10-09 11:32:25 -0700

    Fresh commit

--- crawl_delicious.py
+++ crawl_delicious.py
@@ -14,16 +14,19 @@ for q in open("tags.txt", "r"):
   for o in xrange(0, 10):
     offset = o * 50
     req = BOSS % (q.strip(), offset)
-    resp = urllib.urlopen(req).read()
-    data = simplejson.loads(resp)
-    for r in data["ysearchresponse"]["resultset_web"]:
-      text = r["title"] + " " + r["abstract"]
-      vector = featurize.vectorize(text)
-      tt = r["delicious_toptags"]
-      if len(tt) > 0:
-        tags = tt["tags"]
-        for t in tags:
-          name = t["name"].lower()
-          vector_data.append( (vector, name) )
+    try:
+      resp = urllib.urlopen(req).read()
+      data = simplejson.loads(resp)
+      for r in data["ysearchresponse"]["resultset_web"]:
+        text = r["title"] + " " + r["abstract"]
+        vector = featurize.vectorize(text)
+        tt = r["delicious_toptags"]
+        if len(tt) > 0:
+          tags = tt["tags"]
+          for t in tags:
+            name = t["name"].lower()
+            vector_data.append( (vector, name) )
+    except:
+      pass
 
 cPickle.dump(vector_data, open("vector_data.cpickle", "w"))
